6deb847183499c8f94759fc3c5cd7a22088c5313
Bruno Pintos <bpintos@lithium.com.uy>
2020-02-03 12:55:48 -0300

    onboarding title and subtitle refactored

--- pages/onboarding.js
+++ pages/onboarding.js
@@ -16,12 +16,27 @@ const Subtitle = styled(Typography)`
   }
 `;
 
+const Description = styled(Typography)`
+  && {
+    margin: 0;
+    width: 100%;
+    padding-top: 20px;
+    text-align: center;
+  }
+`;
+
 const Onboarding = () => {
   return (
     <Container>
       <MainHeader title="KB - Knowledge Base" />
       <Title variant="h3">Welcome to KB!</Title>
-      <Subtitle variant="h6">Let's create an article!</Subtitle>
+      <Subtitle variant="h5">
+        Turn your tribal knowledge into easy-to-find answers.
+      </Subtitle>
+      <Subtitle variant="h5">
+        KB is all you need, let's start creating articles!
+      </Subtitle>
+      <Description variant="h6"></Description>
     </Container>
   );
 };
