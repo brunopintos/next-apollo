4eb8bfae71718193368c1f9287b318f5d7c31d1d
Bruno Pintos <bpintos@lithium.com.uy>
2020-02-17 16:19:23 -0300

    modification migration created

---
+++ pages/api/migrations/20200217191452-modification.js
@@ -0,0 +1,47 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Modifications", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      previousContent: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      articleId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Articles"
+          },
+          key: "id"
+        }
+      },
+      authorId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Users"
+          },
+          key: "id"
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Modifications");
+  }
+};
